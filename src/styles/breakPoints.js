434c007652466552ee790f3e2cc332f0d02359a8
nguyenvanty <ty.nguyenvan@hdwebsoft.co>
2021-12-31 13:48:49 +0700

    update

---
+++ src/styles/breakPoints.js
@@ -0,0 +1,12 @@
+export const breakPoint = {
+    XLDesktop: 1680,
+    LDesktop: 1440,
+    MDesktop: 1366,
+    SDesktop: 1200,
+    SmDesktop: 1024,
+    MTablet: 992,
+    SmTablet: 768,
+    LDPhone: 576,
+    MDPhone: 475,
+    SmPhone: 375,
+  };
